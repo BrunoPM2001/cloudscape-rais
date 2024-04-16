b76a91264005e981de42c6441bfd2868d9e188a6
BrunoPM2001 <brunoporras2001@gmail.com>
2024-04-16 11:57:30 -0500

    Using lazy imports to code splitting

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,70 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import "@cloudscape-design/global-styles/index.css";
 import { I18nProvider } from "@cloudscape-design/components/i18n";
 import messages from "@cloudscape-design/components/i18n/messages/all.es";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Gestion_grupos from "./pages/admin/estudios/gestion_grupos/index.jsx";
-import Lineas_investigacion from "./pages/admin/admin/lineas/index.jsx";
-import Usuarios_administrativos from "./pages/admin/admin/usuarios_administrativos/index.jsx";
-import Usuarios_investigadores from "./pages/admin/admin/usuarios_investigadores/index.jsx";
-import Detalle_grupo from "./pages/admin/estudios/gestion_grupos/detalles/index.jsx";
-import Gestion_proyectos_grupos from "./pages/admin/estudios/gestion_proyectos_grupos/index.jsx";
-import Detalle_proyecto_grupo from "./pages/admin/estudios/gestion_proyectos_grupos/detalle/index.jsx";
-import Gestion_proyectos_fex from "./pages/admin/estudios/gestion_proyectos_fex/index.jsx";
-import Informes_tecnicos from "./pages/admin/estudios/informes_tecnicos/index.jsx";
-import Monitoreo from "./pages/admin/estudios/monitoreo/index.jsx";
-import Detalle_monitoreo from "./pages/admin/estudios/monitoreo/detalles/index.jsx";
-import Deudas_proyectos from "./pages/admin/estudios/deudas_proyectos/index.jsx";
-import Gestion_publicacion from "./pages/admin/estudios/gestion_publicaciones/index.jsx";
-import Reporte_estudio from "./pages/admin/reportes/estudio/index.jsx";
-import Reporte_grupo from "./pages/admin/reportes/grupo/index.jsx";
-import Reporte_proyecto from "./pages/admin/reportes/proyecto/index.jsx";
-import Reporte_investigador from "./pages/admin/reportes/investigador/index.jsx";
-import Consolidado_general from "./pages/admin/reportes/consolidado_general/index.jsx";
-import Reporte_presupuesto from "./pages/admin/reportes/presupuesto/index.jsx";
-import Reporte_constancias from "./pages/admin/constancias/reporte_constancias/index.jsx";
+//  Pages
+const Gestion_grupos = lazy(() =>
+  import("./pages/admin/estudios/gestion_grupos/index.jsx")
+);
+const Lineas_investigacion = lazy(() =>
+  import("./pages/admin/admin/lineas/index.jsx")
+);
+const Usuarios_administrativos = lazy(() =>
+  import("./pages/admin/admin/usuarios_administrativos/index.jsx")
+);
+const Usuarios_investigadores = lazy(() =>
+  import("./pages/admin/admin/usuarios_investigadores/index.jsx")
+);
+const Detalle_grupo = lazy(() =>
+  import("./pages/admin/estudios/gestion_grupos/detalles/index.jsx")
+);
+const Gestion_proyectos_grupos = lazy(() =>
+  import("./pages/admin/estudios/gestion_proyectos_grupos/index.jsx")
+);
+const Detalle_proyecto_grupo = lazy(() =>
+  import("./pages/admin/estudios/gestion_proyectos_grupos/detalle/index.jsx")
+);
+const Gestion_proyectos_fex = lazy(() =>
+  import("./pages/admin/estudios/gestion_proyectos_fex/index.jsx")
+);
+const Informes_tecnicos = lazy(() =>
+  import("./pages/admin/estudios/informes_tecnicos/index.jsx")
+);
+const Monitoreo = lazy(() =>
+  import("./pages/admin/estudios/monitoreo/index.jsx")
+);
+const Detalle_monitoreo = lazy(() =>
+  import("./pages/admin/estudios/monitoreo/detalles/index.jsx")
+);
+const Deudas_proyectos = lazy(() =>
+  import("./pages/admin/estudios/deudas_proyectos/index.jsx")
+);
+const Gestion_publicacion = lazy(() =>
+  import("./pages/admin/estudios/gestion_publicaciones/index.jsx")
+);
+const Reporte_estudio = lazy(() =>
+  import("./pages/admin/reportes/estudio/index.jsx")
+);
+const Reporte_grupo = lazy(() =>
+  import("./pages/admin/reportes/grupo/index.jsx")
+);
+const Reporte_proyecto = lazy(() =>
+  import("./pages/admin/reportes/proyecto/index.jsx")
+);
+const Reporte_investigador = lazy(() =>
+  import("./pages/admin/reportes/investigador/index.jsx")
+);
+const Consolidado_general = lazy(() =>
+  import("./pages/admin/reportes/consolidado_general/index.jsx")
+);
+const Reporte_presupuesto = lazy(() =>
+  import("./pages/admin/reportes/presupuesto/index.jsx")
+);
+const Reporte_constancias = lazy(() =>
+  import("./pages/admin/constancias/reporte_constancias/index.jsx")
+);
 
 const router = createBrowserRouter([
   {
